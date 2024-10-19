9030e54fef0e9175f7c6746195f4694cd13fddde
Christian Kemper <christian.kemper@me.com>
2024-10-19 15:32:11 +0100

    setup-tilt-config: Refactored vault_helm_deploy.py to simplify the Helm deployment process and utilize secrets management for unseal key, root token, and service port forward.

--- Tiltfile
+++ Tiltfile
@@ -1,7 +1,3 @@
 load('./tilt/vault_helm_deploy.py', 'vault_helm_deploy') # type: ignore
 
-k8s_resource( # type: ignore
-    vault_helm_deploy(), # type: ignore
-    port_forwards=[8200],
-    extra_pod_selectors=[{'app.kubernetes.io/name': 'vault'}],
-)
+vault_helm_deploy() # type: ignore
--- tilt/vault_helm_deploy.py
+++ tilt/vault_helm_deploy.py
@@ -1,21 +1,21 @@
-load('ext://helm_resource', 'helm_resource', 'helm_repo') # type: ignore
+load('ext://helm_resource', 'helm_resource', 'helm_repo')  # type: ignore
 
 def vault_helm_deploy(values_file='./vault/values.yaml'):
-    helm_repo('hashicorp', 'https://helm.releases.hashicorp.com') # type: ignore
+    helm_repo('hashicorp', 'https://helm.releases.hashicorp.com')  # type: ignore
 
-    helm_resource( # type: ignore
+    helm_resource(  # type: ignore
         'vault',
         'hashicorp/vault',
         namespace="vault",
         pod_readiness='ignore',
         flags=[
-          '--values='+values_file,
-          '--create-namespace',
+            '--values=' + values_file,
+            '--create-namespace',
         ],
         resource_deps=['hashicorp'],
     )
 
-    local_resource( # type: ignore
+    local_resource(  # type: ignore
         'vault-init-and-unseal',
         cmd="""
         # Wait for the Vault pod to be in Running state
@@ -36,8 +36,16 @@ def vault_helm_deploy(values_file='./vault/values.yaml'):
           exit 1
         fi
 
-        # Initialize Vault and store the unseal key in a Kubernetes secret
-        kubectl exec -ti vault-0 --namespace=vault -- vault operator init -key-shares=1 -key-threshold=1 | tee /tmp/vault-init-output.txt | awk '/Unseal Key 1/ {print $4}' | xargs -I {} kubectl create secret generic vault-unseal-secret --namespace=vault --from-literal=unseal-key={}
+        # Initialize Vault and store the unseal key and root token in Kubernetes secrets
+        kubectl exec -ti vault-0 --namespace=vault -- vault operator init -key-shares=1 -key-threshold=1 \
+            | tee /tmp/vault-init-output.txt \
+            | awk '/Unseal Key 1/ {print $4}' \
+            | xargs -I {} kubectl create secret generic vault-unseal-secret --namespace=vault --from-literal=unseal-key={}
+
+        # Extract the root token and store it in a secret
+        cat /tmp/vault-init-output.txt \
+            | awk '/Initial Root Token/ {print $4}' \
+            | xargs -I {} kubectl create secret generic vault-root-token --namespace=vault --from-literal=root-token={}
 
         # Wait for the unseal secret to be created
         while ! kubectl get secret vault-unseal-secret --namespace=vault --no-headers; do
@@ -50,5 +58,9 @@ def vault_helm_deploy(values_file='./vault/values.yaml'):
         """,
         resource_deps=['vault'],
     )
-
-    return 'vault'
+    
+    local_resource( # type: ignore
+        'vault-port-forward',
+        serve_cmd='kubectl port-forward svc/vault 8200:8200 --namespace=vault',
+        resource_deps=['vault'],
+    )    
