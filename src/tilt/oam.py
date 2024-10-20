e4a52cacaacf41e32be6895896d9714a1f91f7e3
Christian Kemper <christian.kemper@me.com>
2024-10-20 14:15:31 +0100

    applied formatting

--- src/tilt/oam.py
+++ src/tilt/oam.py
@@ -1,21 +1,19 @@
-load('ext://helm_resource', 'helm_resource', 'helm_repo')  # type: ignore
-load('ext://namespace', 'namespace_yaml') # type: ignore
+load("ext://helm_resource", "helm_resource", "helm_repo")  # type: ignore
+load("ext://namespace", "namespace_yaml")  # type: ignore
 
-def oam_deploy(values_file='./oam/values.yaml'):
-    helm_repo( # type: ignore
-        'kubevela', 
-        'https://charts.kubevela.net/core'
-    )
 
-    k8s_yaml(namespace_yaml('vela-system')) # type: ignore
+def oam_deploy(values_file="./oam/values.yaml"):
+    helm_repo("kubevela", "https://charts.kubevela.net/core")  # type: ignore
 
-    helm_resource( # type: ignore
-        'vela-core', 
-        'kubevela/vela-core', 
+    k8s_yaml(namespace_yaml("vela-system"))  # type: ignore
+
+    helm_resource(  # type: ignore
+        "vela-core",
+        "kubevela/vela-core",
         namespace="vela-system",
-        pod_readiness='ignore',
+        pod_readiness="ignore",
         flags=[
-            '--values=' + values_file,
+            "--values=" + values_file,
         ],
-        resource_deps=['kubevela'],
+        resource_deps=["kubevela"],
     )
