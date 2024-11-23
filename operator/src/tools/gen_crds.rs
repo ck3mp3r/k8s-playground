95d1df10a02f0230e3c4cb9cbf35f267ae015671
Christian Kemper <christian.kemper@me.com>
2024-11-23 20:02:38 +0000

    --wip-- [skip ci]

--- operator/src/tools/gen_crds.rs
+++ operator/src/tools/gen_crds.rs
@@ -3,10 +3,8 @@ use operator::subscriber::crd::Subscriber;
 use std::fs;
 
 fn main() -> anyhow::Result<()> {
-    // Create the crds/ directory if it doesn't exist
     fs::create_dir_all("../crds")?;
 
-    // Generate and write the Subscriber CRD
     fs::write(
         "../crds/subscriber.yaml",
         serde_yaml::to_string(&Subscriber::crd())?,
