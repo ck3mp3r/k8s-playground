a0d7006776bc2e668ec81698d7e40118fe6fdf72
Christian Kemper <christian.kemper@me.com>
2024-11-23 18:00:09 +0000

    --wip-- [skip ci]

--- operator/src/main.rs
+++ operator/src/main.rs
@@ -2,13 +2,10 @@ mod crd;
 
 mod operator;
 
-use futures::StreamExt;
 use k8s_openapi::chrono;
-use kube::runtime::controller::Controller;
-use kube::{Api, Client};
-use log::{error, info};
+use kube::Client;
 use operator::shared_state::SharedState;
-use operator::subscriber::{error_policy, reconcile};
+use operator::subscriber::controller;
 use std::io::Write;
 use std::sync::Arc;
 #[tokio::main]
@@ -36,23 +33,9 @@ async fn main() -> anyhow::Result<()> {
     // Shared state for the reconciler
     let shared_state = Arc::new(SharedState::new(client.clone()));
 
-    // API for the Subscriber CRD
-    let subscribers: Api<crd::subscriber::Subscriber> = Api::all(client);
+    let controller1 = tokio::spawn(controller(shared_state.clone()));
 
-    // Controller logic
-    Controller::new(subscribers, Default::default())
-        .run(
-            reconcile,            // Reconciliation logic
-            error_policy,         // Error handling logic
-            shared_state.clone(), // Shared context
-        )
-        .for_each(|result| async move {
-            match result {
-                Ok(obj_ref) => info!("Reconciled: {:?}", obj_ref),
-                Err(err) => error!("Error during reconciliation: {:?}", err),
-            }
-        })
-        .await;
+    tokio::try_join!(controller1)?;
 
     Ok(())
 }
